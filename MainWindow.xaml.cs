f845f8a28dda5065a8f16529f0a039119e8b62f0
siddhant1512 <sidsidarora.sa@gmail.com>
2020-03-15 23:07:41 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        AirthmeticViewModel model;
+        public MainWindow()
+        {  
+            InitializeComponent();           
+            model = (AirthmeticViewModel)base.DataContext;
+        }
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            model.updateNumber = double.Parse("7");
+        }
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            model.updateNumber = double.Parse("8");
+        }
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            model.updateNumber = double.Parse("9");
+        }
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            model.updateNumber = double.Parse("4");
+        }
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            model.updateNumber = double.Parse("5");
+        }
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            model.updateNumber = double.Parse("6");
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            model.updateNumber = double.Parse("1");
+        }
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            model.updateNumber = double.Parse("2");
+        }
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            model.updateNumber = double.Parse("3");
+        }
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            model.updateNumber = double.Parse("0");
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            model.operation(1);
+        }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            model.operation(5);
+        }
+        private void equal_Click(object sender, RoutedEventArgs e)
+        {
+            model.operation(0);
+        }
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            model.operation(3);
+        }
+        private void Subtract_Click(object sender, RoutedEventArgs e)
+        {
+            model.operation(2);
+        }
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            model.operation(4);
+        }
+    }
+}
---
+++ ViewModel/AirthmeticViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Calculator
+{
+    class AirthmeticViewModel : INotifyPropertyChanged
+    {
+        public double first =0,second=0,option=0,lastoption=0;
+        bool operand;
+        public AirthmeticViewModel()
+        {         
+            operand = false;
+        }        
+       
+        public double updateNumber
+        {
+            get{return second;}
+            set
+            {
+                if (second == 0)
+                    second = value;
+                else if (operand)
+                {
+                    second = value;
+                    operand = false;
+                }
+                else
+                    second = double.Parse(second.ToString() + value.ToString());
+
+                RaisePropertyChanged("updateNumber");
+            }
+        }
+                
+        public void operation(double index)
+        {
+            option = index;
+            if(option!=5)
+            {if(lastoption !=0)
+            {
+              switch(lastoption)
+              {
+                  case 1:
+                       first += second;
+                       break;
+                  case 2:
+                       first -= second;
+                       break;
+                  case 3:
+                       first *= second;
+                       break;
+                  case 4:
+                       first /= second;
+                       break;
+              }
+              second=0;
+              updateNumber = first;            
+            }
+            else
+            {
+                first = second;           
+            }
+            operand = true; 
+            lastoption = option;
+            }
+            else{
+                operand = true;
+                first=0;
+                lastoption=0;
+                updateNumber = 0;
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
